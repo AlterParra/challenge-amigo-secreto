00f68e7ff7841bbd68ad3041f07172fd3d4edd97
AlterParra <saulperazaparra2412@gmail.com>
2025-02-26 14:14:14 -0600

    Creamos el array que contiene a los amigos, y creamos las funciones agregarAmigos y limpiarCampos

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/*Fucionalidades:
+
+Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.*/
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//array para los nombres de los amigos ingresados
+let amigos = []; 
+
+
+//creamos una función para agregar amigos a la lista
+function agregarAmigo()
+{
+    let nombreAmigo = document.getElementById("amigo"); //instanciamos una variable y le damos el valor del elemento del html que tiene el id amigo
+
+    //validamos si el campo de texto no está vacío 
+    if (nombreAmigo.value != "") {
+        amigos.push(nombreAmigo.value);//si no está vacío, agregamos el nombre al array
+        limpiarCampo();//Llamamos al metodo que limpia el campo del nombre
+    }else
+    {
+        //si está vacío, mostramos una alerta
+        alert("Por favor ingresa un nombre de amigo");
+    }
+
+}
+
+//creamos una función para limpiar el campo de texto después de añadir un nombre
+function limpiarCampo()
+{
+    document.getElementById("amigo").value = "";
+}
